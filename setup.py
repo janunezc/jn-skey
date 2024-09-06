df16c9c52aaa3af383218592d18070dafcab5baf
jn <jose.a.nunez@gmail.com>
2024-09-06 12:43:23 -0600

    first basic intent

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jn-skey",  # Package name as it will appear on PyPI
-    version="1.0.0",
-    author="jnunezc",
-    author_email="jose.a.nunez@gmail.com",
-    description="A tool to type the current datetime when CTRL+D is pressed.",
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/janunezc/jn-skey",  # URL of your project, e.g., GitHub repo
-    packages=find_packages(),
+    name='jn-skey',  # The name of your package
+    version='1.0.0',  # The initial release version
+    packages=find_packages(),  # Automatically find packages in your directory
     entry_points={
         'console_scripts': [
-            'jn-skey=jn_skey.jn_skey:main',  # Command to run the package
+            'jn-skey = jn_skey.jn_skey:main',
         ],
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        'pynput',  # Add any dependencies your package needs
     ],
-    python_requires='>=3.6',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A tool to insert the current datetime using a keyboard shortcut.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/jn-skey',  # Replace with your repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the Python versions supported
 )
